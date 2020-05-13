7900a009afa82bff28dee1b1fe9ec78edb3ff850
Ian <ian@ian>
2020-05-13 17:25:40 -0300

    Update .gitignore, package.json, and 4 more files...

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,45 @@
 <template>
   <div class="ml-5 mr-5 pa-0">
     <v-textarea
-      auto-grow="true"
-      autofocus="true"
+      auto-grow
+      autofocus
       full-width
       flat
       class="custom font-weight-light title"
       v-model="note"
       spellcheck="false"
+      v-on:keyup="save()"
     ></v-textarea>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
   name: "HelloWorld",
 
   data: () => ({
-    note:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus nisl rutrum hendrerit fringilla. Nunc vestibulum ipsum sit amet lacus imperdiet, in commodo eros interdum. Sed non ex mauris. Nunc vestibulum sapien quis nisl scelerisque, et lacinia lorem fringilla. Aliquam scelerisque, ligula et tincidunt venenatis, est ipsum vestibulum urna, ac gravida quam erat in sapien. Ut porta vel nunc a pulvinar. Mauris dignissim diam non lorem imperdiet, vel aliquam ante vulputate. Maecenas vel tincidunt purus. Sed odio tellus, venenatis nec neque nec, ultrices ultricies mi. Quisque aliquam orci rhoncus velit consequat dignissim. Suspendisse egestas commodo tincidunt. Nunc tristique hendrerit nulla a efficitur. Donec id pharetra nunc, non vulputate diam."
-  })
+    note: ""
+  }),
+  methods: {
+    save() {
+      this.ref.set(this.note);
+    }
+  },
+  computed: {
+    ref() {
+      return firebase.database().ref("note");
+    }
+  },
+  async mounted() {
+    const note = await this.ref.once("value");
+    this.note = note.val();
+
+    // this.ref.once("value").then(function(snapshot) {
+    //   console.log(snapshot.val());
+    // });
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4BmJmUbuScvS8thUE0nFgkKYfgVbBFCI",
+  authDomain: "notes-4ffab.firebaseapp.com",
+  databaseURL: "https://notes-4ffab.firebaseio.com",
+  projectId: "notes-4ffab",
+  storageBucket: "notes-4ffab.appspot.com",
+  messagingSenderId: "836432019887",
+  appId: "1:836432019887:web:fe9d404739e254912def5a",
+  measurementId: "G-P1D31QCLJQ"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   vuetify,
