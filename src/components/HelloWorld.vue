acd9f2ac3f852d5643b709283a1e24d57f1a71d8
ian <ian>
2020-08-10 11:11:06 -0300

    change text component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,95 @@
 <template>
   <div lass="pa-0">
-    <tiptap-vuetify v-model="content" :extensions="extensions" :card-props="{ flat: true}" />
+    <v-textarea
+      v-model="content"
+      solo
+      flat
+      full-width
+      auto-grow
+    ></v-textarea>
+    <!-- <tiptap-vuetify v-model="content" :extensions="extensions" :card-props="{ flat: true}" /> -->
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
 
-import {
-  TiptapVuetify,
-  Heading,
-  Bold,
-  Italic,
-  Strike,
-  Underline,
-  Code,
-  Paragraph,
-  BulletList,
-  OrderedList,
-  ListItem,
-  Link,
-  Blockquote,
-  HardBreak,
-  HorizontalRule,
-  History,
-  TodoList,
-  TodoItem
-} from "tiptap-vuetify";
+// import {
+//   TiptapVuetify,
+//   Heading,
+//   Bold,
+//   Italic,
+//   Strike,
+//   Underline,
+//   Code,
+//   Paragraph,
+//   BulletList,
+//   OrderedList,
+//   ListItem,
+//   Link,
+//   Blockquote,
+//   HardBreak,
+//   HorizontalRule,
+//   History,
+//   TodoList,
+//   TodoItem,
+// } from "tiptap-vuetify";
 
 export default {
   name: "HelloWorld",
-  components: { TiptapVuetify },
+  // components: { TiptapVuetify },
   data: () => ({
-    extensions: [
-      History,
-      Blockquote,
-      Link,
-      Underline,
-      Strike,
-      Italic,
-      ListItem,
-      BulletList,
-      OrderedList,
-      [
-        Heading,
-        {
-          options: {
-            levels: [1, 2, 3]
-          }
-        }
-      ],
-      Bold,
-      Code,
-      HorizontalRule,
-      Paragraph,
-      HardBreak,
-      TodoList,
-      TodoItem
-    ],
+    // extensions: [
+    //   History,
+    //   Blockquote,
+    //   Link,
+    //   Underline,
+    //   Strike,
+    //   Italic,
+    //   ListItem,
+    //   BulletList,
+    //   OrderedList,
+    //   [
+    //     Heading,
+    //     {
+    //       options: {
+    //         levels: [1, 2, 3],
+    //       },
+    //     },
+    //   ],
+    //   Bold,
+    //   Code,
+    //   HorizontalRule,
+    //   Paragraph,
+    //   HardBreak,
+    //   TodoList,
+    //   TodoItem,
+    // ],
     content: ""
   }),
   watch: {
     content: {
       handler(val) {
         this.ref.set(val);
-        document.getElementsByTagName("p").forEach(data => {
+        document.getElementsByTagName("p").forEach((data) => {
           data.setAttribute("spellcheck", "false");
         });
-      }
-    }
+      },
+    },
   },
   computed: {
     ref() {
       return firebase.database().ref("note");
-    }
+    },
   },
   async mounted() {
     const note = await this.ref.once("value");
     console.log(note.val());
     this.content = note.val();
 
-    document.getElementsByTagName("p").forEach(data => {
+    document.getElementsByTagName("p").forEach((data) => {
       data.setAttribute("spellcheck", "false");
     });
-  }
+  },
 };
 </script>
